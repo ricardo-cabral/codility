2b283bfb44744ce60d5736cdf933fcc3aaa2fdc9
ricardo <ricardozcabral@gmail.com>
2015-05-22 13:08:44 +0200

    first test commit

---
+++ codility/src/main/java/br/com/rec/codility/App.java
@@ -0,0 +1,13 @@
+package br.com.rec.codility;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
